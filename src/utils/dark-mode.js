6e4d3393e7428cfbfc8c24206a16b1fe1d3ebf24
Kalpesh Koli <kolikalpesh65@gmail.com>
2024-01-10 13:47:22 +0530

    published

---
+++ src/utils/dark-mode.js
@@ -0,0 +1,26 @@
+const darkMode=()=>{
+    const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+    //state
+    const theme = localStorage.getItem('theme');
+    //on mount
+    theme && document.body.classList.add(theme);
+
+    //handlers
+    const handleThemeToggles = () =>{
+        document.body.classList.toggle('light-mode');
+        if(document.body.classList.contains('light-mode')){
+            localStorage.setItem('theme','light-mode');
+
+        }else{
+            localStorage.removeItem('theme');
+            document.body.removeAttribute('class');
+        }
+
+    };
+    //events
+    themeToggleBtns.forEach((btn =>
+        btn.addEventListener('click', handleThemeToggles)
+    ));
+
+};
+export default darkMode;
